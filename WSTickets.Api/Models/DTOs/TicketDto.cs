5a15b45b2f0da57e83b73e28acc326c04261b9b5
Jimmy van Delft <jimmy100403@gmail.com>
2025-04-27 12:10:31 +0200

    feat(tickets): RESTful endpoints for tickets

---
+++ WSTickets.Api/Models/DTOs/TicketDto.cs
@@ -0,0 +1,31 @@
+﻿using WSTickets.Api.Models.Enums;
+
+namespace WSTickets.Api.Models.DTOs;
+
+public class TicketDto
+{
+    public int Id { get; set; }
+    public string Title { get; set; }
+    public string Description { get; set; }
+    public TicketPriority Priority { get; set; }
+    public TicketStatus CurrentStatus { get; set; }
+    public int ReporterId { get; set; }
+    public int? AssigneeId { get; set; }
+    public int CompanyId { get; set; }
+}
+
+public class TicketCreateDto
+{
+    public string Title { get; set; }
+    public string Description { get; set; }
+    public TicketPriority Priority { get; set; }
+    public int CompanyId { get; set; }
+}
+
+public class TicketUpdateDto
+{
+    public string Title { get; set; }
+    public string Description { get; set; }
+    public TicketPriority Priority { get; set; }
+    public TicketStatus CurrentStatus { get; set; }
+}
