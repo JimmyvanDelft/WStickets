001dd2d91ac01d03e4f69e1aa03fb8b2f44c99d5
Jimmy van Delft <jimmy100403@gmail.com>
2025-04-27 11:57:39 +0200

    feat(statusHistory): RESTful endpoints for status

---
+++ WSTickets.Api/Controllers/StatusHistoriesController.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WSTickets.Api.Data;
+using WSTickets.Api.Models.DTOs;
+using WSTickets.Api.Models.Entities;
+using WSTickets.Api.Models.Enums;
+
+namespace WSTickets.Api.Controllers;
+
+[ApiController]
+[Route("api/tickets/{ticketId}/statushistory")]
+[Authorize]
+public class StatusHistoriesController : ControllerBase
+{
+    private readonly AppDbContext _context;
+
+    public StatusHistoriesController(AppDbContext context)
+    {
+        _context = context;
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<StatusHistoryDto>>> GetStatusHistories(int ticketId)
+    {
+        var history = await _context.StatusHistories
+            .Where(sh => sh.TicketId == ticketId)
+            .Select(sh => new StatusHistoryDto
+            {
+                Id = sh.Id,
+                Status = sh.Status,
+                Timestamp = sh.Timestamp,
+                ChangedById = sh.ChangedById
+            })
+            .ToListAsync();
+
+        return Ok(history);
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> AddStatusHistory(int ticketId, [FromBody] StatusHistoryCreateDto dto)
+    {
+        var userId = int.Parse(User.FindFirst(System.Security.Claims.ClaimTypes.NameIdentifier)?.Value);
+
+        var statusHistory = new StatusHistory
+        {
+            TicketId = ticketId,
+            Status = dto.Status,
+            Timestamp = DateTime.UtcNow,
+            ChangedById = userId
+        };
+
+        _context.StatusHistories.Add(statusHistory);
+        await _context.SaveChangesAsync();
+
+        return CreatedAtAction(nameof(GetStatusHistories), new { ticketId = ticketId }, null);
+    }
+}
---
+++ WSTickets.Api/Models/DTOs/StatusHistoryDto.cs
@@ -0,0 +1,16 @@
+﻿using WSTickets.Api.Models.Enums;
+
+namespace WSTickets.Api.Models.DTOs;
+
+public class StatusHistoryDto
+{
+    public int Id { get; set; }
+    public TicketStatus Status { get; set; }
+    public DateTime Timestamp { get; set; }
+    public int ChangedById { get; set; }
+}
+
+public class StatusHistoryCreateDto
+{
+    public TicketStatus Status { get; set; }
+}
