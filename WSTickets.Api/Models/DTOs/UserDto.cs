bedab6b300ca911ac8bbdc179e1b74b4f920832f
Jimmy van Delft <jimmy100403@gmail.com>
2025-04-27 10:42:21 +0200

    feat(users): added RESTful API endpoints for Users

---
+++ WSTickets.Api/Models/DTOs/UserDto.cs
@@ -0,0 +1,29 @@
+﻿namespace WSTickets.Api.Models.DTOs;
+
+public class UserCreateDto
+{
+    public string Username { get; set; }
+    public string Password { get; set; }
+    public string FullName { get; set; }
+    public string Email { get; set; }
+    public int CompanyId { get; set; }
+    public int RoleId { get; set; }
+}
+
+public class UserReadDto
+{
+    public int Id { get; set; }
+    public string Username { get; set; }
+    public string FullName { get; set; }
+    public string Email { get; set; }
+    public string CompanyName { get; set; }
+    public string RoleName { get; set; }
+}
+
+public class UserUpdateDto
+{
+    public string FullName { get; set; }
+    public string Email { get; set; }
+    public int? CompanyId { get; set; }
+    public int? RoleId { get; set; }
+}
