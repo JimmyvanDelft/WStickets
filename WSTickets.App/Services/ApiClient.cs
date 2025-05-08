4c5325cc02bd958c4857460eab7d09b26fbd1213
Jimmy van Delft <jimmy100403@gmail.com>
2025-05-08 16:14:28 +0200

    fix(Android): Fixed API Connection to android

--- WSTickets.App/Services/ApiClient.cs
+++ WSTickets.App/Services/ApiClient.cs
@@ -5,15 +5,35 @@ using System.Net;
 using System.Net.Http.Headers;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Maui.Devices;
 
 namespace WSTickets.App.Services;
 
 public static class ApiClient
 {
-    private static readonly HttpClient _client = new()
+    private static readonly HttpClient _client;
+
+    static ApiClient()
     {
-        BaseAddress = new Uri("http://127.0.0.1:5131/api/")
-    };
+        string baseAddress;
+
+        if (DeviceInfo.Platform == DevicePlatform.Android)
+        {
+            // Android emulator moet 10.0.2.2 gebruiken om host machine te bereiken
+            baseAddress = "http://10.0.2.2:5131/api/";
+        }
+        else
+        {
+            // Windows, iOS simulator, etc.
+            baseAddress = "http://localhost:5131/api/";
+        }
+
+        _client = new HttpClient
+        {
+            BaseAddress = new Uri(baseAddress)
+        };
+    }
+
 
     public static HttpClient Client => _client;
 
